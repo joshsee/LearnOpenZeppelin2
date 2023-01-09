35e6bdbd6e11d98bd3a9b997b930a339abe5b53b
Josh See <ychian@gmail.com>
2023-01-09 17:16:53 +0800

    Calling the contract

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,11 @@ console.log(accounts);
 const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 const Box = await ethers.getContractFactory('Box');
 const box = await Box.attach(address);
+
+// Call the retrieve() function of the deployed Box contract
+const value = await box.retrieve();
+console.log('Box value is', value.toString());
+
 }
   
   main()
