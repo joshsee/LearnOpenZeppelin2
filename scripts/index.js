1c86b809836a00db595f130a3953dde3507eb706
Josh See <ychian@gmail.com>
2023-01-09 17:15:56 +0800

    Getting a contract instance

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,11 @@ async function main () {
     // Retrieve accounts from the local node
 const accounts = await ethers.provider.listAccounts();
 console.log(accounts);
+
+// Set up an ethers contract, representing our deployed Box instance
+const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const Box = await ethers.getContractFactory('Box');
+const box = await Box.attach(address);
 }
   
   main()
