8a149cd5e3dfdd1f2df6d1f85253f92280d8d3de
Josh See <ychian@gmail.com>
2023-01-09 17:19:27 +0800

    Fix value

--- scripts/index.js
+++ scripts/index.js
@@ -17,8 +17,8 @@ console.log('Box value is', value.toString());
 await box.store(23);
 
 // Call the retrieve() function of the deployed Box contract
-const value = await box.retrieve();
-console.log('Box value is', value.toString());
+const value2 = await box.retrieve();
+console.log('Box value is', value2.toString());
 
 }
   
